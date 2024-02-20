df9ad7c352bc7205ae94729a40ae32a1400bfe3e
nata-non <natanon.han@gmail.com>
2024-02-20 17:34:57 +0700

    feat: add props

--- src/views/dashboards/ecommerce/Instancesdetail.tsx
+++ src/views/dashboards/ecommerce/Instancesdetail.tsx
@@ -1,13 +1,13 @@
 // ---- แก้ไขทำ Instant Detail ----
 
 // ** MUI Imports
-import Card from '@mui/material/Card'
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import CardContent from '@mui/material/CardContent'
-import { Box, textAlign } from '@mui/system'
 import { Grid } from '@mui/material'
-import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Divider from '@mui/material/Divider'
+import Typography from '@mui/material/Typography'
+import { Box } from '@mui/system'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
@@ -17,6 +17,7 @@ import { ThemeColor } from 'src/@core/layouts/types'
 
 // ** Custom Components Imports
 import CustomAvatar from 'src/@core/components/mui/avatar'
+import StatusOfflineIcon from '../dashboard/StatusOfflineIcon'
 
 interface DataType {
   icon: string
@@ -29,16 +30,6 @@ interface DataTag {
   tag: string
 }
 
-// interface DataDetail {
-//   id: string
-//   author: string
-//   createdAt: string
-//   expiredAt: string
-//   size: string
-//   type: string
-//   server: string
-//   location: string 
-// }
 
 const data: DataType[] = [
   {
@@ -64,25 +55,20 @@ const dataTag: DataTag[] = [
   }
 ]
 
-const dataDetail: DataDetail[] = [
-  {
-    id: 'JU-03',
-    author: '63070126@it.kmitl.ac.th (ME)',
-    createdAt: '23 April 2023',
-    expiredAt: '24 April 2023',
-    size: 'Nano',
-    type: 'Jupyter Notebook',
-    server: 'DGX-A100',
-    location: 'Faculty of IT, KMITL'
-  }
-]
 
-const renderStats = () => {
+const renderStats = (props: any) => {
+
   return data.map((sale: DataType, index: number) => (
-    <Grid item xs={6} md={3} key={index}>
+    <Grid
+      item
+      xs={5}
+      md={4}
+      key={index}
+      sx={{ flexDirection: 'row', display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+    >
       <Box key={index} sx={{ display: 'flex', alignItems: 'center' }}>
         <CustomAvatar skin='light' variant='rounded' color={sale.color} sx={{ mr: 2, width: 42, height: 42 }}>
-          <Icon icon={sale.icon} fontSize='1.5rem' />
+          {props.status === 1 ? <Icon icon={sale.icon} fontSize='1.5rem' /> : <StatusOfflineIcon />}
         </CustomAvatar>
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
           <Typography variant='h5'>{sale.stats}</Typography>
@@ -95,22 +81,68 @@ const renderStats = () => {
 
 const renderTag = () => {
   return dataTag.map((sale: DataTag, index: number) => (
-    <Typography key={index} sx={{ mr: 6, color: 'text.secondary', border: "1px solid none", backgroundColor: "#A8AAAE29", borderRadius: '4px', paddingBlock: '5px', paddingInline: '10px'}}>
+    <Typography
+      key={index}
+      sx={{
+        mr: 2,
+        color: 'text.secondary',
+        border: '1px solid none',
+        backgroundColor: '#A8AAAE29',
+        borderRadius: '4px',
+        paddingBlock: '5px',
+        paddingInline: '10px'
+      }}
+    >
       {sale.tag}
     </Typography>
   ))
 }
 
-// const renderDetail = () => {
-//   return dataDetail.map((sale: DataDetail, index: number))
-// }
-const InstantDetail = () => {
+const renderDetail = (props: any) => {
+  return <Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>ID: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontWeight: 600, fontSize: '15px' }}>{props.id}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Author: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px' }}>{props.author}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Created At: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px' }}>{props.createdAt}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Expired At: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px', color: '#FF9F43' }}>{props.endDate}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Size: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px' }}>{props.size}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Type: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px' }}>{props.type}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Server: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px' }}>{props.server}</Typography>
+    </Grid>
+    <Grid spacing={5} container sx={{ ml: 0.08, mb: 5, display: 'flex', flexDirection: 'row' }}>
+      <Typography sx={{ mb: 2, fontWeight: 600, fontSize: '15px' }}>Location: </Typography>
+      <Typography sx={{ ml: 2, mb: 2, fontSize: '15px' }}>{props.location}</Typography>
+    </Grid>
+  </Grid>
+}
+
+const InstantDetail = ({ props }: any) => {
+
   return (
     <Card sx={{ position: 'relative' }} style={{ lineHeight: '10' }}>
       <CardContent>
         {/* change text */}
         <Typography variant='h3' sx={{ mb: 4 }} style={{ textAlign: 'center' }}>
-          Aroi Compute
+          {props.name}
         </Typography>
 
         {/* add tag */}
@@ -118,19 +150,85 @@ const InstantDetail = () => {
           {renderTag()}
         </Box>
 
-        <Grid spacing={7} container sx={{ mb: 5 }} style={{ display: 'flex', justifyContent: 'center' }}>
-          {renderStats()}
+        <Grid spacing={2} container sx={{ mb: 5 }} style={{ display: 'flex', justifyContent: 'center' }}>
+          {renderStats(props)}
         </Grid>
-        
-        <Divider sx={{ mb: 5 }}/>
 
-        <Typography variant='subtitle1' style={{ color: '#8692D0' }}>DETAILS</Typography>
-        {/* <Typography sx={{ mb: 2, color: 'text.secondary' }}>Best seller of the month</Typography>
-        <Typography variant='h4' sx={{ mb: 0.75, color: 'primary.main' }}>
-          $48.9k
+        <Divider sx={{ mb: 5 }} />
+
+        <Typography variant='subtitle1' sx={{ mb: 7 }} style={{ color: '#8692D0' }}>
+          DETAILS
         </Typography>
-        <Button variant='contained'>View Sales</Button> */}
-        {/* <Illustration width={116} alt='congratulations john' src='/images/cards/congratulations-john.png' /> */}
+
+        <Grid>{renderDetail(props)}</Grid>
+
+        <Divider sx={{ mb: 5 }} />
+
+        <Grid container xs={12} md={12} spacing={1} style={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            type='submit'
+            variant='contained'
+            sx={{
+              mr: 2,
+              mb: 2,
+              width: '45%',
+              backgroundColor: '#7367F029',
+              color: '#7367F0',
+              '&:hover': {
+                backgroundColor: '#7367F029',
+                color: '#FFFFFF'
+              }
+            }}
+          >
+            Extend
+          </Button>
+
+          <Button
+            type='submit'
+            variant='contained'
+            sx={{
+              mb: 2,
+              width: '45%',
+              backgroundColor: '#FF9F4329',
+              color: '#FF9F43',
+              '&:hover': {
+                backgroundColor: '#FF9F4329',
+                color: '#FFFFFF'
+              }
+            }}
+          >
+            Restart
+          </Button>
+          <Button
+            type='submit'
+            variant='contained'
+            sx={{
+              mr: 2,
+              mb: 2,
+              width: '45%',
+              backgroundColor: '#EA545529',
+              color: '#EA5455',
+              '&:hover': {
+                backgroundColor: '#EA545529',
+                color: '#FFFFFF'
+              }
+            }}
+          >
+            Stop
+          </Button>
+
+          <Button
+            type='submit'
+            variant='contained'
+            color='error'
+            sx={{
+              mb: 2,
+              width: '45%'
+            }}
+          >
+            Delete
+          </Button>
+        </Grid>
       </CardContent>
     </Card>
   )
